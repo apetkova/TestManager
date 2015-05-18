4b9bfbc48ebc84056e999a1c171721dc76794483
apetkova <aneta.v.petkova@gmail.com>
2015-05-18 23:26:45 +0300

    new

---
+++ TestManagerAPI/src/main/java/com/apetkova/tm/details/UploadFileResponse.java
@@ -0,0 +1,9 @@
+package com.apetkova.tm.details;
+
+public class UploadFileResponse extends EntityDetails {
+	String name;
+
+	// testname => type; includename => testname; description = "TestNG";
+	// automated = true
+
+}
